bcb5b8f9af40b9e2b422fdc236fa13c236ed4242
Rosinsd <152635516+Rosinsd@users.noreply.github.com>
2023-12-15 10:31:31 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+/**
+ * Created by Jayden Smith
+ * 
+ * December 15 2023 
+ * 
+ * This code is a simple counter when button A is pressed it goes up one when button B is pressed it goes down 1
+ */
 input.onButtonPressed(Button.A, function () {
     counter += 1
 })
